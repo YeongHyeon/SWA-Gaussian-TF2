383ad024e91a8756846c54952a75af68cb8ff4ab
YeongHyeon Park <young200405@gmail.com>
2021-10-01 22:04:43 +0900

    update

--- source/tf_process.py
+++ source/tf_process.py
@@ -7,7 +7,6 @@ def training(agent, dataset, batch_size, epochs, c_update, s_avg):
 
     print("\n** Training to %d epoch | Batch size: %d" %(epochs, batch_size))
     iteration = 0
-    loss_best = 1e+12
     for epoch in range(epochs):
 
         list_loss = []
@@ -21,10 +20,7 @@ def training(agent, dataset, batch_size, epochs, c_update, s_avg):
         list_loss = np.asarray(list_loss)
         loss_tmp = np.average(list_loss)
 
-        if(loss_best > loss_tmp):
-            loss_best = loss_tmp
-            agent.save_params(model='model_1_best_loss')
-        agent.save_params(model='model_0_finepocch')
+        agent.save_params(model='model_finepocch')
 
         if(epoch % c_update == 0):
             agent.swag(num_model=epoch/c_update, training=True)
